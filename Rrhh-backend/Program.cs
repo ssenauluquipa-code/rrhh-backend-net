f2f5f31a25cb30e7a68132672ee3d8d5ae15a880
senauluquipas <senauluquipas@gmail.com>
2025-12-21 16:23:19 -0400

    Fix: swagger

--- Rrhh-backend/Program.cs
+++ Rrhh-backend/Program.cs
@@ -216,7 +216,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "API NEBULA v1");
+        }
+        );
 }
 // En producción, no se ejecuta UseSwagger ni UseSwaggerUI
 
